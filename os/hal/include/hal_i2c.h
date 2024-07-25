195fdcb883a3d31295e1f01b74ccd9cce5f8c88a
Alexandre Bustico <alexandre.bustico@enac.fr>
2024-07-25 11:09:57 +0200

    mark i2c translit functions nodiscard

--- os/hal/include/hal_i2c.h
+++ os/hal/include/hal_i2c.h
@@ -146,12 +146,16 @@ extern "C" {
   msg_t i2cStart(I2CDriver *i2cp, const I2CConfig *config);
   void i2cStop(I2CDriver *i2cp);
   i2cflags_t i2cGetErrors(I2CDriver *i2cp);
+  
+  [[nodiscard]]
   __attribute__((access (write_only, 5, 6), access (read_only,  3, 4)))
   msg_t i2cMasterTransmitTimeout(I2CDriver *i2cp,
                                  i2caddr_t addr,
                                  const uint8_t *txbuf, size_t txbytes,
                                  uint8_t *rxbuf, size_t rxbytes,
                                  sysinterval_t timeout);
+
+  [[nodiscard]]
   __attribute__((access (write_only, 3, 4)))
   msg_t i2cMasterReceiveTimeout(I2CDriver *i2cp,
                                 i2caddr_t addr,
@@ -162,10 +166,13 @@ extern "C" {
   void i2cReleaseBus(I2CDriver *i2cp);
 #endif
 #if I2C_SUPPORTS_SLAVE_MODE == TRUE
+  [[nodiscard]]
   msg_t i2cSlaveMatchAddress(I2CDriver *i2cp, i2caddr_t  i2cadr);
+  [[nodiscard]]
   __attribute__((access (write_only, 3, 4)))
   msg_t i2cSlaveReceiveTimeout(I2CDriver *i2cp, uint8_t *rxbuf,
                                size_t rxbytes, sysinterval_t timeout);
+  [[nodiscard]]
   __attribute__((access (read_only, 3, 4)))
   msg_t i2cSlaveTransmitTimeout(I2CDriver *i2cp, const uint8_t *txbuf,
                                 size_t txbytes, sysinterval_t timeout);
