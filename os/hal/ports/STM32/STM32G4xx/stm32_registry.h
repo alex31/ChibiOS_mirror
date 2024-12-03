07ae363a4c51c72616e0c5b7ce9558ce78e1e42f
Alexandre Bustico <alexandre.bustico@enac.fr>
2024-12-03 23:40:18 +0100

    fix number of flash memort banks for G491 serie

--- os/hal/ports/STM32/STM32G4xx/stm32_registry.h
+++ os/hal/ports/STM32/STM32G4xx/stm32_registry.h
@@ -372,11 +372,11 @@
 #define STM32_EXTI_IMR2_MASK                0xFFFFFF3CU
 
 /* Flash attributes.*/
-#define STM32_FLASH_NUMBER_OF_BANKS         2
+#define STM32_FLASH_NUMBER_OF_BANKS         1
 #define STM32_FLASH_SECTOR_SIZE             2048U
 #define STM32_FLASH_LINE_SIZE               8U
 #if !defined(STM32_FLASH_SECTORS_PER_BANK) || defined(__DOXYGEN__)
-#define STM32_FLASH_SECTORS_PER_BANK        128 /* Maximum, can be redefined.*/
+#define STM32_FLASH_SECTORS_PER_BANK        256 /* Maximum, can be redefined.*/
 #endif
 
 /* GPIO attributes.*/
